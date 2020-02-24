3b4bf136fe3e5d1bfcf5f8769b35c0630fcf80f1
barja8 <antony.barja8@gmail.com>
2020-02-24 15:27:10 -0500

    MiguelAngel

--- MiguelAngel/Script /Pp_months.R
+++ MiguelAngel/Script /Pp_months.R
@@ -3,9 +3,13 @@ library(tidyverse)
 # Lectura de datos  -------------------------------------------------------
 
 read_csv('Data/estacion.csv') %>% 
-  separate(col = FECHA,into = c('día','mes','año'),sep = '/') %>%
-  group_by(mes, año) %>% 
-  summarise(Pp = sum(VALOR)) -> newdata
+  separate(col = FECHA,into = c('día','mes','año'),sep = '/')%>%
+  mutate(mes = as.factor((as.numeric(mes)))) %>% 
+  group_by(año,mes) %>% 
+  summarise(Pp = sum(VALOR)) %>% 
+  spread(mes,Pp) -> newdata
 
+colnames(newdata) <- c('años',month.name)
+ 
 write.csv(newdata,'Resultado/Newtable.csv')
 
