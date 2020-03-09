cac187dfa7cc4f1cbc93b60dc9192fcc5d4eb0db
barja8 <antony.barja8@gmail.com>
2020-03-09 10:58:02 -0500

    Junior

--- Junior/Script/Pregunta02.R
+++ Junior/Script/Pregunta02.R
@@ -1,16 +1,14 @@
 # Lectura de datos --------------------------------------------------------
-library(tidyverse)
-
 nombres_CA <- read.table('Data/nombres_CA.txt', sep = ',')
 nombres_TX <- read.table('Data/nombres_TX.TXT', sep = ',')
 
 data <- rbind(nombres_CA,nombres_TX)
 
-contrybb <- function(df,año){
-  n <- subset(df , year == año)
+contrybb <- function(year_min, year_max){
+  n <- subset(df , year == año & sex == F)
   p <- n[,c('year','Name','Num')]
   return(p)
-  }
+}
 
 # Opcional con tidyverse : 
 # rbind(nombres_CA,nombres_TX) %>% 
