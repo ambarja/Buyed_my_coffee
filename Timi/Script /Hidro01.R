f49b51c523e31349b808d9df669ac9315a1a5d41
barja8 <antony.barja8@gmail.com>
2020-03-10 11:58:25 -0500

    Timi

--- Timi/Script /Hidro01.R
+++ Timi/Script /Hidro01.R
@@ -8,8 +8,8 @@ my.formula <-  data$COTA ~ poly(data$VOLUMEN,3)
 data %>% 
 ggplot(aes(x = VOLUMEN,y = COTA)) + 
   geom_line(col = 'blue') + 
-  scale_y_continuous(breaks = seq(4677.9,4679.6,length.out = 8)) +
-  scale_x_continuous(breaks = seq(0.054,2332.591,length.out = 8))+
+  scale_y_continuous(breaks = seq(4677.9, 4679.6, length.out = 8)) +
+  scale_x_continuous(breaks = seq(0.054, 2332.591, length.out = 8))+
   geom_point(shape = 15) + 
   stat_poly_eq(aes(label = ..eq.label..), formula = my.formula, 
                parse = TRUE) +
@@ -23,7 +23,10 @@ last_plot()
 
 # Export to graphic -------------------------------------------------------
 
-ggplot2::ggsave(filename = 'Plot/Hidro.png',plot = g1,width = 10,height = 4)
+ggplot2::ggsave(filename = 'Plot/Hidro.png',
+                plot = g1,
+                width = 10,
+                height = 4)
 
 
 
