0c73fce80b5cea7f9dc42bc7b7496bee7ddf006b
barja8 <antony.barja8@gmail.com>
2020-03-20 13:52:56 -0500

    MiguelAngle Gráfico de Pp vs Temp

--- MiguelAngel/Script /Grafico_Pp_vsTemp.R
+++ MiguelAngel/Script /Grafico_Pp_vsTemp.R
@@ -9,7 +9,7 @@ read_csv('Data/YACAR.csv')%>%
   ggplot(aes(x = id)) + 
   geom_line(aes(y = Temp, colour = "Temperatura")) +
   scale_x_continuous(breaks = seq(1,12,by = 1),
-                     labels = c(month.abb[8:12],month.abb[1:7])) +
+                     labels = c(month.abb[9:12],month.abb[1:8])) +
   geom_line(aes(y = Pp, colour = "Precipitación")) +
   scale_colour_manual(values = c("blue", "red")) + 
   scale_y_continuous(breaks = seq(0,150,10),
